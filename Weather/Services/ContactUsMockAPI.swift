3dc3b0f95f01db2b4eac637aae48f12eb1abfc3f
praveen <praveen_kumar_somu@techpass.gov.sg>
2022-09-04 17:50:01 +0800

    Display random number as request id for user on contact us submission

--- Weather/Services/ContactUsMockAPI.swift
+++ Weather/Services/ContactUsMockAPI.swift
@@ -10,7 +10,8 @@ import Foundation
 
 class ContactUsMockAPI: ContactUsStoreProtocol {
 	func submitContactUsDetails(_ request: ContactUsModel.Request) async throws -> Data {
-		guard let data = "Details submitted successfully, we will contact you soon.".data(using: .utf8) else {
+		let userRequestId = arc4random_uniform(1000000)
+		guard let data = "Details submitted successfully, we will contact you soon. Save your request Id for further communication \(userRequestId).".data(using: .utf8) else {
 			throw ContactUsError.genericError
 		}
 		return data
